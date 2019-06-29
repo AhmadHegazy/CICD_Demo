65e14a4584d2a0faa91e62787952dd4251f5c938
Mohamed Atef AboelQasem <mada_fci@hotmail.com>
2019-06-29 17:27:03 +0300

    test error

--- src/main/java/com/cicd/demo/CiCdDemoApplication.java
+++ src/main/java/com/cicd/demo/CiCdDemoApplication.java
@@ -7,8 +7,8 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class CiCdDemoApplication {
 
-	public static void main(String[] args) {
-		//SpringApplication.run(CiCdDemoApplication.class, args);
+	//public static void main(String[] args) {
+		SpringApplication.run(CiCdDemoApplication.class, args);
 	}
 
 }
